35ce3e1f24b7440c943e84948c724433866752c9
Mehmet Efe Uzun <mefeuzunn@gmail.com>
2026-04-06 17:13:34 +0300

    feat: remove Coinpayu affiliate banner from site

--- src/components/RightSidebarAds.tsx
+++ src/components/RightSidebarAds.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { AdPlaceholder } from "./AdPlaceholder";
-import { AffiliateBanner } from "./AffiliateBanner";
 
 export function RightSidebarAds() {
   return (
     <aside className="right-sidebar sticky-sidebar">
-      {/* Affiliate Banner */}
-      <div style={{ marginBottom: "1.5rem" }}>
-        <AffiliateBanner />
-      </div>
-      
       {/* 300x250 Rectangle Ad */}
       <AdPlaceholder type="rectangle" />
       
